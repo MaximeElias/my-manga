db63e903a968d108ef78785a405c14b03728a306
Maxime ELIAS <maxime_elias@ens.univ-artois.fr>
2025-03-28 12:12:44 +0100

    Initial commit

--- app/src/main/java/iutlens/android/mymangalist/model/Anime.kt
+++
@@ -1,9 +0,0 @@
-package iutlens.android.mymangalist.model
-
-data class Anime(
-    val id: Int,
-    val title: String,
-    val state: String,
-    val seasons: List<Season>,
-    val coverUrl: String
-)
--- app/src/main/java/iutlens/android/mymangalist/model/Season.kt
+++
@@ -1,7 +0,0 @@
-package iutlens.android.mymangalist.model
-
-data class Season(
-    val seasonNumber: Int,
-    val episodesWatched: Int,
-    val totalEpisodes: Int
-)
